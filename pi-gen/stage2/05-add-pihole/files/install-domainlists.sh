b54fad7f42cace75ecd38d720a5cc431a82201cc
Brad Barnes <bbarnes@gmail.com>
2024-08-12 12:30:08 -0400

    Update install-domainlists.sh
    added YouTube to domain lists

--- pi-gen/stage2/05-add-pihole/files/install-domainlists.sh
+++ pi-gen/stage2/05-add-pihole/files/install-domainlists.sh
@@ -5,11 +5,12 @@
 export PATH="$PATH:/usr/bin:/usr/local/bin/"
 
 #domainlists=(streaming_video)
-domainlists=(all social gaming streaming_video)
+domainlists=(all social gaming streaming_video youtube)
 all="(.*)"
 gaming="(\.|^)nintendo\.com$|(\.|^)nintendo\.net$|(\.|^)roblox\.com$|(\.|^)rbxcdn\.com$|(\.|^)crazygames\.com$|(\.|^)poki\.com$|(\.|^)tracker\.gg$|(\.|^)gamepix\.com$|(\.|^)shellshockers\.net$|(\.|^)nintendo\.net$|(\.|^)playstation\.net$"
 social="(\.|^)snapchat\.com$|(\.|^)sc-cdn\.net$|(\.|^)sc-cdn\.net$|(\.|^)t.co$|(\.|^)twitter\.com$|(\.|^)twttr\.com$|(\.|^)reddit\.com$|(\.|^)redd.it$|(\.|^)whatsapp\.com$|(\.|^)discordapp\.com$|(\.|^)signal\.org$"
 streaming_video="(\.|^)netflix\.com$|(\.|^)netflix\.ca$|(\.|^)netflix\.net$|(\.|^)dssott\.com$|(\.|^)disneyplus\.com$|(\.|^)disney-plus\.net$|(\.|^)primevideo\.com$|(\.|^)amazonvideo\.com$"
+youtube="(\.|^)youtube\.com$|(\.|^)googlevideo\.com$|(\.|^)googleapis\.com$"
 
 for domainlist in ${domainlists[@]} ; do
     echo "adding domainlist $domainlist/${!domainlist} to gravity.db"
