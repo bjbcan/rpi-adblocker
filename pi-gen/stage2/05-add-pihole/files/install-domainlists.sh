3eed874a1fcb0bc21ab538b97cf2a48dd3b9ebce
Brad Barnes <bbarnes@gmail.com>
2024-12-22 15:59:52 -0500

    Update install-domainlists.sh
    added facebookinsta

--- pi-gen/stage2/05-add-pihole/files/install-domainlists.sh
+++ pi-gen/stage2/05-add-pihole/files/install-domainlists.sh
@@ -4,13 +4,15 @@
 #
 export PATH="$PATH:/usr/bin:/usr/local/bin/"
 
-#domainlists=(streaming_video)
-domainlists=(all social gaming streaming_video youtube)
+# (1 2 3 4 5 6)
+domainlists=(all social gaming streaming_video youtube facebookinsta)
 all="(.*)"
 gaming="(\.|^)nintendo\.com$|(\.|^)nintendo\.net$|(\.|^)roblox\.com$|(\.|^)rbxcdn\.com$|(\.|^)crazygames\.com$|(\.|^)poki\.com$|(\.|^)tracker\.gg$|(\.|^)gamepix\.com$|(\.|^)shellshockers\.net$|(\.|^)nintendo\.net$|(\.|^)playstation\.net$"
 social="(\.|^)snapchat\.com$|(\.|^)sc-cdn\.net$|(\.|^)sc-cdn\.net$|(\.|^)t.co$|(\.|^)twitter\.com$|(\.|^)twttr\.com$|(\.|^)reddit\.com$|(\.|^)redd.it$|(\.|^)whatsapp\.com$|(\.|^)discordapp\.com$|(\.|^)signal\.org$"
 streaming_video="(\.|^)netflix\.com$|(\.|^)netflix\.ca$|(\.|^)netflix\.net$|(\.|^)dssott\.com$|(\.|^)disneyplus\.com$|(\.|^)disney-plus\.net$|(\.|^)primevideo\.com$|(\.|^)amazonvideo\.com$"
 youtube="(\.|^)youtube\.com$|(\.|^)googlevideo\.com$|(\.|^)googleapis\.com$"
+facebookinsta=“(\.|^)facebook\.com$|(\.|^)fbcdn\.net$”
+
 
 for domainlist in ${domainlists[@]} ; do
     echo "adding domainlist $domainlist/${!domainlist} to gravity.db"
